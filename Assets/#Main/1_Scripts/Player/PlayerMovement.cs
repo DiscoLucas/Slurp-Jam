90f7db6ae9f26e08c4062033b8cb4148a3faa885
BingoMango <103103876+HurtigSkelet@users.noreply.github.com>
2026-01-13 21:45:21 +0100

    Added dash

--- Assets/#Main/1_Scripts/Player/PlayerMovement.cs
+++ Assets/#Main/1_Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Runtime.Serialization;
 using UnityEditor.Callbacks;
 using UnityEngine;
@@ -7,10 +8,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController _characterController;
     public float moveModifier;
+    public float dashPower;
+    public float dashDuration;
     public float gravityModifier;
     private Vector2 _moveDirection;
 
     public InputActionReference Move;
+    public InputActionReference Dash;
 
     void Update()
     {
@@ -21,5 +25,38 @@ public class PlayerMovement : MonoBehaviour
     {
         float gravity = gravityModifier * Time.deltaTime;
         _characterController.Move(new Vector3(_moveDirection.x * moveModifier, gravity, _moveDirection.y * moveModifier));
+    }
+
+    void OnEnable()
+    {
+        Dash.action.started += OnDash;
+    }
+
+    void OnDisable()
+    {
+        Dash.action.started -= OnDash;
+    }
+
+    private void OnDash(InputAction.CallbackContext context)
+    {
+        //check if on cooldown
+        Debug.Log("Dashing");
+        StartCoroutine(DashEnum());
+    }
+
+    IEnumerator DashEnum()
+    {
+        float myTime = dashDuration; 
+        float elapsedTime = 0f;
+
+        Vector3 dashDirection =
+            new Vector3(_moveDirection.x, 0f, _moveDirection.y).normalized * dashPower;
+
+        while (elapsedTime < myTime)
+        {
+            _characterController.Move(dashDirection * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
