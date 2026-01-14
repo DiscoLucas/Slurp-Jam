dcde42d1e59fa1d68e3d9c183bf2841dddb4df66
BingoMango <103103876+HurtigSkelet@users.noreply.github.com>
2026-01-14 15:43:17 +0100

    He throws dåse nu

---
+++ Assets/#Main/2_Prefabs/Useables/Can/CanThrow.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using Unity.AppUI.UI;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CanThrow : MonoBehaviour
+{
+    private float timer = 2f;
+    public int Damage;
+    public float travelSpeed;
+    private float hitStrenghThreshold = 1f;
+    Vector3 direction;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        direction = (transform.position - GameObject.FindWithTag("Player").transform.position).normalized;
+
+        rb.AddForce(direction*travelSpeed, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && rb.linearVelocity.magnitude > hitStrenghThreshold)
+        {
+            EnemytClass enemy = collision.gameObject.GetComponent<EnemytClass>();
+            if (enemy != null)
+            {
+                enemy.EnemyTakeDamage(Damage);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
