63035b9fd398582883b27c33b3cde3cf6896cb28
Kiliopo <94614380+Kiliopo@users.noreply.github.com>
2026-01-14 20:15:28 +0100

    UI stuff

---
+++ Assets/#Main/1_Scripts/Player/UI_AmmoText.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UI_AmmoText : MonoBehaviour
+{
+
+    public ProjectileSpawner projectileSpawner;
+    public TextMeshProUGUI text;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        RefreshAmmoText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RefreshAmmoText();
+    }
+
+    public void RefreshAmmoText()
+    {
+        if (projectileSpawner.usesAmmo)
+        {
+            text.text = projectileSpawner.ammo.ToString();
+        }
+        else
+        {
+            text.text = "∞";
+        }
+    }
+}
